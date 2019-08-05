d49d68e35576b9dd0d6ec6cee0a7620e42015155
Milen Khanal <milenkhanal@gmail.com>
2019-08-05 22:08:23 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,9 +21,10 @@ def bonus
   }
 
   #code your solution here
-  
+  def 
  epic_tragedy[:montague][:hero][:status] = "dead"
 epic_tragedy[:capulet][:heroine][:status] = "dead"
   epic_tragedy
 end
   
+end
